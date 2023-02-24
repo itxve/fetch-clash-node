8ecf2865b622736617ec6f1a6d731c924a4a509f
tianxueren <1770664399@qq.com>
2023-02-24 16:03:50 +0800

    add update log

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function zore(t) {
   return t > 10 ? `${t}` : `0${t}`;
 }
 function wf(name, data) {
-  fs.writeFileSync(path.join(__dirname, "node", `${name}.yaml`), data);
+  fs.writeFileSync(path.join(__dirname, "node", name), data);
 }
 //2023/02/20230224.yaml
 
@@ -24,14 +24,14 @@ let y = zore(d.getFullYear());
 let m = zore(d.getMonth() + 1);
 let day = zore(d.getDate());
 let timeStr = [y, m, `${y}${m}${day}.yaml`].join("/");
-console.log("hour:" + d.getHours());
 (async () => {
   try {
     for (let node of nodes) {
       let res = await fetch(node.hostUrl + timeStr).then((res) => res.text());
       console.log(`[ ${node.hostUrl + timeStr} ] is ok`);
-      wf(node.name, res);
+      wf(`${node.name}.yaml`, res);
     }
+    wf(`update.txt`, [y, m, day, zore(d.getHours())].join("/"));
   } catch (error) {
     console.log("error :" + error);
   }
