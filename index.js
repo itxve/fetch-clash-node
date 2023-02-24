62b998b0807ea165cc44a57c4be8f8e3e24d013f
tianxueren <1770664399@qq.com>
2023-02-24 15:19:52 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+function zore(t) {
+  return t > 10 ? `${t}` : `0${t}`;
+}
+function wf(name, data) {
+  fs.writeFileSync(path.join(__dirname, "node", `${name}.yaml`), data);
+}
+//2023/02/20230224.yaml
+
+let nodes = [
+  {
+    name: "NodeFree",
+    hostUrl: "https://nodefree.org/dy/",
+  },
+  {
+    name: "ClashNode",
+    hostUrl: "https://clashnode.com/wp-content/uploads/",
+  },
+];
+
+let d = new Date();
+let y = zore(d.getFullYear());
+let m = zore(d.getMonth() + 1);
+let day = zore(d.getDate());
+let timeStr = [y, m, `${y}${m}${day}.yaml`].join("/");
+
+(async () => {
+  try {
+    for (let node of nodes) {
+      let res = await fetch(node.hostUrl + timeStr).then((res) => res.text());
+      console.log(`[ ${node.hostUrl + timeStr} ] is ok`);
+      wf(node.name, res);
+    }
+  } catch (error) {
+    console.log("error :" + error);
+  }
+})();
