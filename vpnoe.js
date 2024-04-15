8616c25188453b25e8652ccb03070c22d026d0b8
tianxueren <1770664399@qq.com>
2024-04-15 11:25:53 +0800

    update

--- vpnoe.js
+++ vpnoe.js
@@ -3,20 +3,20 @@ const { wf } = require("./utils");
 (async () => {
   await fetch("https://vpnoe.com/mfjd.html", {
     headers: {
-      Cookie:
-        "579e449d7eec66b888e62c8140a39ba6__typecho_remember_mail=1770231%40qq.com",
+      Cookie: `579e449d7eec66b888e62c8140a39ba6__typecho_remember_author=iiitre; 579e449d7eec66b888e62c8140a39ba6__typecho_remember_mail=gawe%40gmail.com`,
     },
   }).then(async (res) => {
     const text = await res.text();
     if (res.status === 200) {
-      let [_, link] = text.match("Clash： (.*)clash=1");
-      link += "clash=1";
+      let [_, link] = text.match("Clash订阅地址：(.*)\\<\\/code");
+      console.log("link:", link);
+      // link += "clash=1";
       console.log("link:::::", link);
       await fetch(link)
         .then((r) => r.text())
         .then((text) => {
           if (text) {
-             wf("vpnoe.yaml", text);
+            wf("vpnoe.yaml", text);
           }
         });
     }
